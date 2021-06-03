b1b9ec6628b7c90105f96240a3da7c22180b55b3
bus710 <bus710@gmail.com>
2021-06-03 16:52:39 -0700

    wip

--- src/matrixd/src/main.rs
+++ src/matrixd/src/main.rs
@@ -8,8 +8,10 @@ use matrix::*;
 
 use crate::senders::async_knocker_run;
 
+use actix_web::{web, App, HttpServer};
+
 #[tokio::main]
-async fn main() {
+pub async fn main() -> std::io::Result<()> {
     println!("Start matrix service");
 
     let signal_rx = signal_catcher().unwrap();
@@ -20,7 +22,22 @@ async fn main() {
     async_knocker_run(matrix_tx.clone(), signal_rx.clone()).await;
     sh_runner.run().await;
 
-    server::run();
+    let local = tokio::task::LocalSet::new();
+    let sys = actix_web::rt::System::run_in_tokio("server", &local);
+    let server_res = HttpServer::new(|| App::new().route("/", web::get().to(server::hello)))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await?;
+    sys.await?;
+    Ok(server_res)
 
-    println!("End matrix service");
+    // HttpServer::new(|| {
+    //     App::new()
+    //         .service(server::hello)
+    //         .service(server::echo)
+    //         .route("/hey", web::get().to(server::manual_hello))
+    // })
+    // .bind("0.0.0.0:8080")?
+    // .run()
+    // .await
 }
--- src/matrixd/src/server.rs
+++ src/matrixd/src/server.rs
@@ -1,3 +1,15 @@
-pub fn run() {
-    println!("..");
+use actix_web::{post, HttpResponse, Responder};
+
+pub async fn hello() -> impl Responder {
+    // HttpResponse::Ok().body("Hello world!")
+    ""
+}
+
+#[post("/echo")]
+pub async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+pub async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
 }
