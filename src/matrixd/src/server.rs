1c3c0499f3cca8e005eea43e95d7a90d1256ce59
bus710 <bus710@gmail.com>
2021-06-03 23:12:15 -0700

    wip

--- src/matrixd/src/server.rs
+++ src/matrixd/src/server.rs
@@ -14,36 +14,6 @@ pub struct Data {
     pub b1: [u8; 3],
 }
 
-pub async fn run(
-    matrix_tx: crossbeam_channel::Sender<matrix::Data>,
-    signal_rx: crossbeam_channel::Receiver<()>,
-) {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
-    let matrix_tx_filter = warp::any().map(move || matrix_tx.clone());
-
-    // let cleanup = warp::path::end().map(|| {
-    //     let d = matrix::Data::new();
-    //     matrix_tx.send(d).unwrap();
-    // });
-
-    // let routes = hello.with(warp::cors().allow_any_origin());
-    let routes = warp::any()
-        .and(warp::path("v1"))
-        .and(warp::path("data"))
-        .and(warp::path::end())
-        .and(json_body())
-        .and(matrix_tx_filter.clone())
-        .and_then(hello2).or(hello);
-
-    let (addr, server) =
-        warp::serve(routes).bind_with_graceful_shutdown(([0, 0, 0, 0], 8080), async move {
-            let _ = signal_rx.recv();
-        });
-    println!("{}", addr);
-    tokio::task::spawn(server).await.unwrap();
-}
-
 fn json_body() -> impl Filter<Extract = (Data,), Error = warp::Rejection> + Clone {
     println!("json_body");
     warp::body::content_length_limit(1024 * 16).and(warp::body::json())
@@ -56,4 +26,30 @@ pub async fn hello2(
     println!("hello2 - {:?}", d);
 
     Ok(warp::reply::with_status("hello2", http::StatusCode::OK))
+}
+
+pub async fn run(
+    matrix_tx: crossbeam_channel::Sender<matrix::Data>,
+    signal_rx: crossbeam_channel::Receiver<()>,
+) {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let matrix_tx_filter = warp::any().map(move || matrix_tx.clone());
+
+    let hello2 = warp::post()
+        .and(warp::path("v1"))
+        .and(warp::path("data"))
+        .and(warp::path::end())
+        .and(json_body())
+        .and(matrix_tx_filter.clone())
+        .and_then(hello2);
+
+    let routes = hello2.or(hello).with(warp::cors().allow_any_origin());
+
+    let (addr, server) =
+        warp::serve(routes).bind_with_graceful_shutdown(([0, 0, 0, 0], 8080), async move {
+            let _ = signal_rx.recv();
+        });
+    println!("{}", addr);
+    tokio::task::spawn(server).await.unwrap();
 }
